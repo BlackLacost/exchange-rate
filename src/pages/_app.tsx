4e583ad138d29e3111988aba4f8b39e2e124949f
Ilya Lisin <blacklacost@gmail.com>
2023-04-14 21:20:03 +0300

    Форма с выбором валют

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
+import Head from "next/head";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Currency Exchange Rates</title>
+        <meta name="description" content="💭" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
