1bfc7bc798ea2900c8d179a042fa71cc62bd1029
Ilya Lisin <blacklacost@gmail.com>
2023-04-16 19:39:57 +0300

    Отфильтровал валюты, которые не работают с Intl.number

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,12 +1,23 @@
+import { toCurrency } from "~/utils/currency";
+
 export const fetchAllCurrencies = async () => {
   const res = await fetch(
     "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
   );
   const data = (await res.json()) as Record<string, string>;
-  return Object.entries(data).map(([k, v]) => ({
-    value: k,
-    name: v,
-  }));
+  return (
+    Object.entries(data)
+      // Отфильтровал валюты, которые не работают с Intl.number
+      .filter(([value]) => {
+        try {
+          const result = toCurrency(100, value);
+          return Boolean(result);
+        } catch (e) {
+          return false;
+        }
+      })
+      .map(([value, name]) => ({ value, name }))
+  );
 };
 
 export const fetchExchangeRateFromTo = async (from: string, to: string) => {
